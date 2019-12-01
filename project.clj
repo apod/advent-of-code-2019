52721d52075f51d49ad45ae042ccc16f65a0fb16
Alex Podaras <apodaras@gmail.com>
2019-12-01 15:20:37 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject advent-of-code-2019 "0.1.0-SNAPSHOT"
+  :description "My solutions for AoC 2019"
+  :url "http://adventofcode.com/2019"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]])
