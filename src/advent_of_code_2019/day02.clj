73fc4dc0e2e75b7730de4979e3c39357051e28ac
Alex Podaras <apodaras@gmail.com>
2019-12-02 22:39:59 +0200

    Add a slower and faster implementation of run-intcode

--- src/advent_of_code_2019/day02.clj
+++ src/advent_of_code_2019/day02.clj
@@ -7,26 +7,44 @@
 
 (defn run-intcode
   ([coll]
-   (let [input (int-array coll)]
-     (loop [pos 0]
-       (let [op (aget input pos)]
-         (case op
-           1 (let [[p1 p2 p3] [(aget input (+ pos 1))
-                               (aget input (+ pos 2))
-                               (aget input (+ pos 3))]]
-               (aset input p3
-                     (+ (aget input p1) (aget input p2)))
-               (recur (+ pos 4)))
-           2 (let [[p1 p2 p3] [(aget input (+ pos 1))
-                               (aget input (+ pos 2))
-                               (aget input (+ pos 3))]]
-               (aset input p3
-                     (* (aget input p1) (aget input p2)))
-               (recur (+ pos 4)))
-           99 (vec input)
-           :unknown-op)))))
+   (let [op-fn (fn [f input pos]
+                 (let [end-pos (+ pos 4)
+                       [p1 p2 p3] (mapv input (range (inc pos) end-pos))
+                       op-res (f (get input p1) (get input p2))
+                       new-input (assoc input p3 op-res)]
+                   [end-pos new-input]))]
+     (loop [[pos input] [0 coll]]
+       (case (get input pos)
+         1 (recur (op-fn + input pos))
+         2 (recur (op-fn * input pos))
+         99 input
+         :unknown-op))))
   ([coll noun verb]
    (run-intcode (assoc coll 1 noun 2 verb))))
+
+;; 2nd faster version
+#_(defn run-intcode
+  ([coll]
+   (let [input (int-array coll)]
+     (loop [pos 0]
+       (case (aget input pos)
+         1 (let [[p1 p2 p3] [(aget input (+ pos 1))
+                             (aget input (+ pos 2))
+                             (aget input (+ pos 3))]]
+             (aset-int input p3
+                       (+ (aget input p1) (aget input p2)))
+             (recur (+ pos 4)))
+         2 (let [[p1 p2 p3] [(aget input (+ pos 1))
+                             (aget input (+ pos 2))
+                             (aget input (+ pos 3))]]
+             (aset-int input p3
+                       (* (aget input p1) (aget input p2)))
+             (recur (+ pos 4)))
+         99 (vec input)
+         :unknown-op))))
+  ([coll noun verb]
+   (run-intcode (assoc coll 1 noun 2 verb))))
+
 
 (defn noun-and-verb-for-output [input desired-output]
   (loop [noun 0 verb 0]
